55749d38b5578d8ba1c0be4697e38620992f617f
Markus Gärtner <markus.gaertner@it-agile.de>
2013-02-12 19:06:17 +0100

    zweiter Test läuft jetzt auch durch

--- src/AmpelZustaende.java
+++ src/AmpelZustaende.java
@@ -1,11 +1,14 @@
 
 public class AmpelZustaende {
 
+	private String state;
+
 	public void setVorherigerZustand(String state) {
-		
+		this.state = state;
 	}
 	
 	public String naechsterZustand() {
-		return "Rot, Gelb";
+		if ("Rot".equals(state)) return "Rot, Gelb";
+		return "Grün";
 	}
 }
